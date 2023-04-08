25aea7f9a3f483b016595c617b120d9af61590e8
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-04-07 21:47:20 -0500

    listo

--- test/tests.py
+++ test/tests.py
@@ -1,9 +1,6 @@
 import sys
 import os
 
-sys.path.insert(0,
-                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from test.PeleaTest import PeleaTest
 
 if __name__ == "__main__":
