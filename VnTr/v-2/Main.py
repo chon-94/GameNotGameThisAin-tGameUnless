f6aa3102e9d66f50eed54bd77beac70874e2e217
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-04-24 00:34:24 -0500

    Personajes

--- VnTr/v-2/Main.py
+++ VnTr/v-2/Main.py
@@ -1,13 +1,13 @@
 # Importamos la clase Jugador desde el archivo jugador.py
 from jugador import Jugador
 
-
+# Definimos la función main
 def main():
     # Creamos una instancia de la clase Jugador
     jugador = Jugador()
 
     # Creamos una instancia de la clase Enemigo, pasando la vida del jugador como argumento
-    enemigo = Enemigo(jugador.vida * 2)
+    #enemigo = Enemigo(jugador.vida * 2)
 
     # Llamamos al método asignar_puntos para que el jugador pueda asignar puntos a sus habilidades
     jugador.asignar_puntos()
@@ -16,4 +16,9 @@ def main():
     jugador.mostrar_habilidades()
 
     # Mostramos la vida del enemigo
-    enemigo.mostrar_vida()
+    #enemigo.mostrar_vida()
+
+# Verificamos si este archivo se está ejecutando directamente, en lugar de ser importado como un módulo
+if __name__ == '__main__':
+    main()
+
